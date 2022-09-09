226c0278ba68d6c339f66894d682f50ead9f7881
dhellstern <git@daniel.hellstern.org>
2022-09-09 03:05:22 +0000

    Wrapped EEPROM in function for scoping Switched to proxies Switched away from io call

--- autoflash/autoflash.lua
+++ autoflash/autoflash.lua
@@ -2,25 +2,28 @@
 -- note that writing the EEPROM will overwrite this program, so the client should send a copy of this program
 -- every time if this functionality needs to be preserved.
 
-local modem = component.proxy(component.list("modem")())
-local eeprom = component.eeprom
+(function ()
+  local modem = component.proxy(component.list("modem")())
+  local drone = component.proxy(component.list("drone")())
+  local eeprom = component.proxy(component.list("eeprom")())
 
--- listen for incoming flash connections
-modem.open(122)
-io.write("Autoflash: My address is " + modem.address + "\n")
+  -- listen for incoming flash connections
+  modem.open(122)
+  drone.setStatusText("My address is "..modem.address.."\n")
 
--- when a remote flash message is received, write the data to the EEPROM
-event.listen("modem_message", function(_, _, from, port, _, message, rom)
-    if port == 122 and message == "remote_flash" then
-      -- write the revceived ROM to the EEPROM
-      eeprom.set(rom)
+  -- when a remote flash message is received, write the data to the EEPROM
+  event.listen("modem_message", function(_, _, from, port, _, message, rom)
+      if port == 122 and message == "remote_flash" then
+        -- write the received ROM to the EEPROM
+        eeprom.set(rom)
 
-      -- success beeps
-      for i = 1, 3 do
-        computer.beep(1000, 0.1)
+        -- success beeps
+        for i = 1, 3 do
+          computer.beep(1000, 0.1)
+        end
+
+        -- reboot into the new ROM
+        computer.shutdown(true)
       end
-
-      -- reboot into the new ROM
-      computer.shutdown(true)
-    end
-end)
+  end)
+end)()
